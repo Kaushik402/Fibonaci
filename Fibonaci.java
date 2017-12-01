a3f64ffab7940815e037fbc9c2772340c3d4a74f
Kaushik402 <32414829+Kaushik402@users.noreply.github.com>
2017-12-01 15:07:30 +0530

    Create Fibonaci.java

---
+++ Fibonaci.java
@@ -0,0 +1,29 @@
+//0,1,1,2,3,5,8
+
+
+class Fibonaci
+{public static void main(String a[])
+	{
+		int b=10;
+		int i,f[]=new int [50];
+
+		for(i=0;i<=b;i++) 
+		{
+			if(i==1)
+			{
+				f[i]=1;
+			}
+		
+			if(i==0)
+			{
+				f[i]=0;
+			}			
+
+			else
+			{
+				f[i]=(f[i-2])+(f[i-1]);
+			}
+				System.out.println("Fibonaci:="+f[i]);
+		}
+	}
+}
